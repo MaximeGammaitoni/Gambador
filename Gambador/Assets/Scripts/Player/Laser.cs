a209331b1fbbe7deae7da2936e7a69aa446119f5
Maxime Gammaitoni <maxime.gammaitoni@gmail.com>
2019-11-02 20:44:15 +0100

    feature/add_ray_beetwen_player_and_mousePosition

---
+++ Gambador/Assets/Scripts/Player/Laser.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LineRenderer lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 mousePos = GetMousePosition();
+        RaycastHit hit;
+        Vector3 fromPosition = transform.position;
+        Vector3 toPosition = mousePos;
+        Vector3 direction = toPosition - fromPosition;
+        Debug.DrawRay(fromPosition, direction, Color.red);
+        if (Physics.Raycast(fromPosition, direction, out hit))
+        {
+            if(hit.transform.name== "Obstacle")
+            {
+               
+                lr.SetColors(Color.red, Color.red);
+                Debug.Log(hit.transform.name);
+            }
+
+        }
+        else
+        {
+            lr.SetColors(Color.green, Color.green);
+        }
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, mousePos);
+
+    }
+
+    Vector3 GetMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = GameManager.singleton.cameraManager.RaycastToMousePosition();
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Ground")
+            {
+                Vector3 objectHit = new Vector3(hit.point.x, hit.point.y+0.3f, hit.point.z);
+                return  objectHit;
+            }
+        }
+
+        return transform.position;
+    }
+}
