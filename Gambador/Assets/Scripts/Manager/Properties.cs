0075e835fafa2a1802a8881cd03f880dcb51f5cb
Touba john <touba.john@gmail.com>
2019-11-02 19:06:37 +0100

    [IMP] Add damage manager

---
+++ Gambador/Assets/Scripts/Manager/Properties.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    public int hp;
+    public int moveSpeed;
+    public int damage;
+    public float attackRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Gambador/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private Damage damage;
+    private Properties properties;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = this.gameObject.GetComponent<Damage>();
+        properties = this.gameObject.GetComponent<Properties>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            damage.MakeCircular(properties.damage);
+        }
+    }
+}
