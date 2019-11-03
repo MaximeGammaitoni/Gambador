998b4ff5f4f604cea050262f3afb7f7fb439c725
Maxime Gammaitoni <maxime.gammaitoni@gmail.com>
2019-11-03 18:25:47 +0100

    feature/move_player_with_vfx_delay

--- Gambador/Assets/Scripts/Manager/RaycastManager.cs
+++ Gambador/Assets/Scripts/Manager/RaycastManager.cs
@@ -6,12 +6,19 @@ public class RaycastManager
 {
     public List<string> WalkablesTags;
     public List<string> Obstaclestags;
+    private GameObject player;
+    private LineRenderer lr;
+    private float playerHeight;
     public RaycastManager()
     {
         WalkablesTags = new List<string>();
         Obstaclestags = new List<string>();
         WalkablesTags.Add("Ground");
         Obstaclestags.Add("Obstacle");
+        player = GameObject.Find("Player").gameObject;
+        lr = player.transform.Find("Laser").GetComponent<LineRenderer>();
+        playerHeight = player.GetComponent<MeshRenderer>().bounds.size.y;
+        GameManager.GameUpdate += MovingRaycastWithLineRenderer;
     }
     public Vector3 GetMousePosition(Vector3 initialPosition)
     {
@@ -24,17 +31,15 @@ public class RaycastManager
                 Vector3 objectHit = new Vector3(hit.point.x, hit.point.y + 0.3f, hit.point.z);
                 return objectHit;
             }
-
         }
-
         return initialPosition;
     }
 
-    public void MovingRaycastWithLineRenderer(Transform objectToMoveTransform, LineRenderer lr, float objectToMoveHeight)
+    public void MovingRaycastWithLineRenderer()
     {
-        Vector3 mousePos = GameManager.singleton.RaycastManager.GetMousePosition(objectToMoveTransform.position);
+        Vector3 mousePos = GameManager.singleton.RaycastManager.GetMousePosition(player.transform.position);
         RaycastHit hit;
-        Vector3 fromPosition = objectToMoveTransform.position;
+        Vector3 fromPosition = player.transform.position;
         Vector3 toPosition = mousePos;
         Vector3 direction = toPosition - fromPosition;
         float distance = Vector3.Distance(toPosition, fromPosition);
@@ -49,7 +54,7 @@ public class RaycastManager
                 if (Input.GetMouseButtonDown(0)) // not an obstacle in trajectory, player can move
                 {
                     Vector3 objectHit = new Vector3(mousePos.x, mousePos.y , mousePos.z);
-                    objectToMoveTransform.position = objectHit;
+                    GameManager.singleton.MovingPlayerManager.StartMovingPlayer(objectHit);
                 }
             }
         }
@@ -58,13 +63,13 @@ public class RaycastManager
             if (Input.GetMouseButtonDown(0))//nothing in trajectory, player can move
             {
                 Vector3 objectHit = new Vector3(mousePos.x, mousePos.y , mousePos.z);
-                objectToMoveTransform.position = objectHit;
+                GameManager.singleton.MovingPlayerManager.StartMovingPlayer(objectHit);
             }
 
             lr.startColor = Color.green;
             lr.endColor = Color.green;
         }
-        lr.SetPosition(0, objectToMoveTransform.position);
+        lr.SetPosition(0, player.transform.position);
         lr.SetPosition(1, mousePos);
     }
 }
