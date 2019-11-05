159904c4325b9b6075d29901d3e0a61df6af99ee
Maxime Gammaitoni <maxime.gammaitoni@gmail.com>
2019-11-05 12:59:49 +0100

    upgrade deplacement system

--- Gambador/Assets/Scripts/Manager/RaycastManager.cs
+++ Gambador/Assets/Scripts/Manager/RaycastManager.cs
@@ -27,6 +27,7 @@ public class RaycastManager
     {
         RaycastHit hit;
         Ray ray = GameManager.singleton.CameraManager.RaycastToMousePosition();
+        
         if (Physics.Raycast(ray, out hit))
         {
             if (WalkablesTags.Contains(hit.transform.tag))//can move
@@ -45,18 +46,20 @@ public class RaycastManager
         Vector3 fromPosition = player.transform.position;
         Vector3 toPosition = mousePos;
         Vector3 direction = toPosition - fromPosition;
+        Vector3 toPosition2D = new Vector3(toPosition.x, 0, toPosition.z);
+        Vector3 fromPosition2D = new Vector3(fromPosition.x, 0, fromPosition.z);
+        float distance2D = Vector3.Distance(toPosition2D, fromPosition2D);
         float distance = Vector3.Distance(toPosition, fromPosition);
-        float radius = 7.45f;
-        if (distance > radius) //If the distance is less than the radius, it is already within the circle.
+        float radius = 15.45f;
+        if (distance2D > radius) //If the distance is less than the radius, it is already within the circle.
         {
-
             Vector3 fromOriginToObject = toPosition - fromPosition; //~GreenPosition~ - *BlackCenter*
-            fromOriginToObject *= radius / distance; //Multiply by radius //Divide by Distance
+            fromOriginToObject *= radius / distance2D; //Multiply by radius //Divide by Distance
             mousePos = fromPosition + fromOriginToObject; //*BlackCenter* + all that Math
-            if (Physics.Raycast(mousePos, Vector3.down, out hit, 1000))
-                mousePos.y = hit.transform.position.y+playerHeight;
+            Debug.DrawRay(new Vector3(mousePos.x, mousePos.y + 2.5f, mousePos.z), Vector3.down, Color.red);
+            if (Physics.Raycast(new Vector3(mousePos.x, mousePos.y + 2.5f, mousePos.z), Vector3.down, out hit, 1000))
+                mousePos.y = hit.transform.position.y + playerHeight;
         }
-
 
         lr.SetPosition(0, player.transform.position);
         lr.SetPosition(1, mousePos);
@@ -78,49 +81,14 @@ public class RaycastManager
         }
         else
         {
-
             lr.startColor = Color.green;
             lr.endColor = Color.green;
 
             if (Input.GetMouseButtonDown(0)) // not an obstacle in trajectory, player can move
             {
-
                 Vector3 objectHit = new Vector3(mousePos.x, mousePos.y, mousePos.z);
                 GameManager.singleton.MovingPlayerManager.StartMovingPlayer(objectHit);
-
-
-
             }
-
         }
-
     }
-
-    private bool PointInsideSphere(Vector3 point, Vector3 center, float radius)
-    {
-        return Vector3.Distance(point, center) < radius;
-    }
-    public bool checkPoint(int radius, int x, int y,
-                  float percent, float startAngle)
-    {
-
-        // calculate endAngle 
-        float endAngle = 360 / percent + startAngle;
-
-        // Calculate polar co-ordinates 
-        float polarradius =
-                    (float)Math.Sqrt(x * x + y * y);
-
-        float Angle = (float)Math.Atan(y / x);
-
-        // Check whether polarradius is less then  
-        // radius of circle or not and Angle is  
-        // between startAngle and endAngle or not 
-        if (Angle >= startAngle && Angle <= endAngle
-                            && polarradius < radius)
-            return true;
-        else
-            return false;
-    }
-
 }
