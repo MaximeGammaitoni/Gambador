59327b9998eca1fdf89cb65217f38c9e2c1455e6
Maxime Gammaitoni <maxime.gammaitoni@gmail.com>
2019-11-06 22:27:51 +0100

    feature/manage_player_range -

---
+++ Gambador/Assets/Scripts/Config.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Config 
+{
+    public static float PlayerSpeed = 60;
+    public static float TimeScale = 1;
+    public static float Range = 1;
+    public static float RangeIncrementBy = 1;
+    public static float MaxRange = 50;
+}
