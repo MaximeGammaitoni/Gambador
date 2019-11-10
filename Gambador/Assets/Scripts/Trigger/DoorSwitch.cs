f039dc40609f731bbc038c9c60851683fa4634fa
Maxime Gammaitoni <maxime.gammaitoni@gmail.com>
2019-11-10 03:19:22 +0100

    feature/add_doors

---
+++ Gambador/Assets/Scripts/Trigger/DoorSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+    public Vector3 addDest;
+    public GameObject Door;
+    private bool isOpen = false;
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player" && !isOpen)
+        {
+            StartCoroutine(OpenDoor());
+            
+        }
+        isOpen = true;
+    
+    }
+
+    IEnumerator OpenDoor()
+    {
+        var dest = addDest + Door.transform.position;
+        float ratioSpeed = 0;
+        while (Door.transform.position != dest)
+        {
+            Door.transform.position = Vector3.Lerp(Door.transform.position, dest,  ratioSpeed);
+            ratioSpeed += Time.deltaTime * Config.TimeScale;         
+            yield return 0;
+        }
+    }
+}
